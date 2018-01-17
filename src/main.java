a803ee2919b179d355a3cb7bfbcb043c49ba09be
Florian Molenaars <f.molenaars@hotmail.com>
2018-01-17 12:54:21 +0100

    hallo
    Signed-off-by: Florian Molenaars <f.molenaars@hotmail.com>

--- src/main.java
+++ src/main.java
@@ -1,6 +1,6 @@
 
-public class main {
-    public main(String[] args) {
+public  class main {
+    public static void main(String[] args) {
     	Simulator sim = new Simulator();
     	sim.run();
     }
