482de1785f240acfdcc9b14d73fd2c7d17eec0bc
danielgeerts7 <daniel.geerts@live.nl>
2018-01-26 13:43:49 +0100

    Added resolution and fullscreen possibility

--- src/view/MainView.java
+++ src/view/MainView.java
@@ -2,6 +2,7 @@ package view;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
+import java.awt.Dimension;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -13,9 +14,14 @@ import model.Model;
  * explain here what this class does
  * 
  * @author danielgeerts7
- * @version 22-01-2018
+ * @version 26-01-2018
  */
 public class MainView {	
+	
+	// A 16:9 resolution
+	private int width = 1536;
+	private int height = 864;
+	private boolean fullscreen = false;
 	
 	private JFrame frame;
 	private Model model;
@@ -30,6 +36,9 @@ public class MainView {
 	
 	public MainView(Model model, JFrame frame, int numberOfFloors, int numberOfRows, int numberOfPlaces) {
 		this.frame = frame;
+		if (fullscreen) {
+			frame.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+		}
 		this.model = model;
 		
 		carParkView = new CarParkView(model, numberOfFloors, numberOfRows, numberOfPlaces);
@@ -44,16 +53,20 @@ public class MainView {
 		buttonPane.add(plusHundredTicks);
 		
 		Container contentPane = frame.getContentPane();
-        contentPane.add(carParkView, BorderLayout.CENTER);
-        contentPane.add(buttonPane, BorderLayout.SOUTH);
-        contentPane.add(textInfoView, BorderLayout.NORTH);
-        
+		contentPane.setPreferredSize(new Dimension(width, height));
+		contentPane.add(carParkView, BorderLayout.CENTER);
+		contentPane.add(buttonPane, BorderLayout.SOUTH);
+		contentPane.add(textInfoView, BorderLayout.NORTH);
+                
         frame.pack();
         frame.setVisible(true);
 
         updateView();
     }
 
+	/**
+	 * This method updates every view (probably all JPanel)
+	 */
     public void updateView() {
         carParkView.updateView();
         textInfoView.updateInfo(model);
