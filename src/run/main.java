1f9e253f21cdd3436dc53f058001f3b06cae63cd
danielgeerts7 <daniel.geerts@live.nl>
2018-02-06 13:21:59 +0100

    Fixed possible crash on Mac OS

--- src/run/main.java
+++ src/run/main.java
@@ -20,7 +20,7 @@ public  class main {
 	 * **/
 	public static void main(String[] args) {
 		ImageIcon img = new ImageIcon("media/The-J-Team_icon.png");
-		StartWindow startWindow = new StartWindow("The J-Team™ Parking Simulator");
+		StartWindow startWindow = new StartWindow("The J-Team\u2122 Parking Simulator");
 		startWindow.setIconImage(img.getImage());
 
 	}
